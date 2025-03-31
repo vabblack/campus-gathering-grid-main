614bf1b4e76ef3757abae3fc14685f16b48ed1e6
Vaibhav <105680163+vabblack@users.noreply.github.com>
2025-03-31 18:32:14 +0530

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -62,7 +62,7 @@ export const events: Event[] = [
     location: 'Recreation Center Courts',
     organizer: 'Campus Recreation',
     category: 'Sports' as EventCategory,
-    image: 'https://images.unsplash.com/photo-1518514379462-5c11e75348c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+    image: 'https://happenings.lpu.in/wp-content/uploads/2019/08/Basketball6.jpg',
     ticketPrice: 10,
     featured: false
   },
